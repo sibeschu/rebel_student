91ed15e0743b89bc2a982f66a93268815ee678bf
sibeschu <ben.schuhknecht@gmx.net>
2025-12-04 09:28:51 +0100

    fixed missing gripper spam

--- src/igus_student/igus_student/student_control.py
+++ src/igus_student/igus_student/student_control.py
@@ -79,46 +79,40 @@ def main():
         _robot = StudentRobotController()
 
         print("Robot controller initialized")
-
         print("Example program started")
 
-        # Roboter zu Position (0.4, 0.2, 0.3) mit Orientierung (π, 0, 0) bewegen
-        success = _robot.move_and_wait(0.4, 0.0, 0.1, 3.14, 0.0, 0.0)
-        if success:
-            print("Bewegung erfolgreich!")
-        else:
-            print("Bewegung fehlgeschlagen!")
+        """ !!! Z KOORDINATE CA +0.12 FÜR STIFTHALTER !!! """
+        # Roboter zu Position (0.4, 0.0, 0.15) mit Orientierung (~π, 0, 0) bewegen
 
-        roll, pitch, yaw = euler_from_quaternion([0.998722, 0.0427602, 0.0197104, 0.0183904])
+        # roll, pitch, yaw = euler_from_quaternion([0.998722, 0.0427602, 0.0197104, 0.0183904])
         # Move to first position - automatically waits
-        #_robot.move_to_pose(0.4, 0.2, 0.3, roll, pitch, yaw)
-        print("Reached position 1", roll, pitch, yaw)
+        #_robot.move_to_pose(0.4, 0.0, 0.3, roll, pitch, yaw)
+        # print("Reached position 1", roll, pitch, yaw)
 
-        # Move to second position - automatically waits
-        _robot.move_and_wait(0.4, 0.0, 0.015, 3.14, 0.0, 0.0)
-        print("move to pose Reached position 1")
+        # _robot.move_and_wait(0.4, 0.0, 0.15, 3.14, 0.0, 0.0)
+        # print("Reached position 1")
 
-        igus.move_ee_vertical(_robot, delta_z=0.1)
+        # igus.move_ee_vertical(_robot, delta_z=0.1)
         
-        _robot.move_and_wait(0.4, 0.1, 0.015, 3.14, 0.0, 0.0)
-        print("move to pose Reached position 2")
+        # _robot.move_and_wait(0.4, 0.1, 0.15, 3.14, 0.0, 0.0)
+        # print("Reached position 2")
 
-        igus.move_ee_vertical(_robot, delta_z=0.1)
+        # igus.move_ee_vertical(_robot, delta_z=0.1)
         
-        _robot.move_and_wait(0.4, 0.2, 0.015, 3.14, 0.0, 0.0)
-        print("move to pose Reached position 3")
+        # _robot.move_and_wait(0.4, 0.2, 0.15, 3.14, 0.0, 0.0)
+        # print("Reached position 3")
 
-        igus.move_ee_vertical(_robot, delta_z=0.1)
+        # igus.move_ee_vertical(_robot, delta_z=0.1)
         
-        _robot.move_and_wait(0.4, 0.3, 0.015, 3.14, 0.0, 0.0)
-        print("move to pose Reached position 4")
+        # _robot.move_and_wait(0.4, 0.3, 0.15, 3.14, 0.0, 0.0)
+        # print("Reached position 4")
 
-        igus.move_ee_vertical(_robot, delta_z=0.1)
+        # igus.move_ee_vertical(_robot, delta_z=0.1)
 
-        _robot.move_and_wait(0.4, 0.0, 0.15, 3.14, 0.0, 0.0)
-        print("Reached position 4")
+        # _robot.move_and_wait(0.4, 0.0, 0.15, 3.14, 0.0, 0.0)
+        # print("Reached position 5")
 
-        igus.move_ee_vertical(_robot, delta_z=0.1)
+        # igus.move_ee_vertical(_robot, delta_z=0.1)
    
         print("PROGRAM COMPLETED")
         
